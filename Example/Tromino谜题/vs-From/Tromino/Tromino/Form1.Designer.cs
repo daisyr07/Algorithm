cc4d56d219ee6479f6934d044fe669612a237470
Doris19967 <40756453+Doris19967@users.noreply.github.com>
2020-01-26 11:43:47 +0800

    Add files via upload

---
+++ Example/Tromino谜题/vs-From/Tromino/Tromino/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Tromino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonEnd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonStart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonStart.Location = new System.Drawing.Point(12, 551);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(109, 58);
+            this.buttonStart.TabIndex = 11;
+            this.buttonStart.Text = "start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonResult.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonResult.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonResult.Location = new System.Drawing.Point(127, 551);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(85, 58);
+            this.buttonResult.TabIndex = 11;
+            this.buttonResult.Text = "result";
+            this.buttonResult.UseVisualStyleBackColor = false;
+            this.buttonResult.Click += new System.EventHandler(this.buttonResult_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonPrev.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonPrev.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonPrev.Location = new System.Drawing.Point(218, 551);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(101, 58);
+            this.buttonPrev.TabIndex = 11;
+            this.buttonPrev.Text = "Pre_step";
+            this.buttonPrev.UseVisualStyleBackColor = false;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonNext.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonNext.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonNext.Location = new System.Drawing.Point(325, 551);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(111, 58);
+            this.buttonNext.TabIndex = 11;
+            this.buttonNext.Text = "Nex_step";
+            this.buttonNext.UseVisualStyleBackColor = false;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonEnd
+            // 
+            this.buttonEnd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonEnd.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonEnd.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonEnd.Location = new System.Drawing.Point(442, 551);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(90, 58);
+            this.buttonEnd.TabIndex = 11;
+            this.buttonEnd.Text = "restart";
+            this.buttonEnd.UseVisualStyleBackColor = false;
+            this.buttonEnd.Click += new System.EventHandler(this.buttonEnd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SeaShell;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(601, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 26);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "size";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.SeaShell;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(601, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 32);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "缺块的X";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.SeaShell;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(601, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 32);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "缺块的Y";
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.BackColor = System.Drawing.Color.Snow;
+            this.domainUpDown1.Font = new System.Drawing.Font("Source Code Pro", 10.8F);
+            this.domainUpDown1.Items.Add("1");
+            this.domainUpDown1.Items.Add("2");
+            this.domainUpDown1.Items.Add("4");
+            this.domainUpDown1.Items.Add("8");
+            this.domainUpDown1.Items.Add("16");
+            this.domainUpDown1.Items.Add("32");
+            this.domainUpDown1.Items.Add("64");
+            this.domainUpDown1.Items.Add("128");
+            this.domainUpDown1.Items.Add("256");
+            this.domainUpDown1.Location = new System.Drawing.Point(724, 148);
+            this.domainUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(75, 26);
+            this.domainUpDown1.TabIndex = 15;
+            this.domainUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.domainUpDown1.SelectedItemChanged += new System.EventHandler(this.domainUpDown1_SelectedItemChanged);
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.BackColor = System.Drawing.Color.Snow;
+            this.textBoxX.Font = new System.Drawing.Font("Source Code Pro", 10.8F);
+            this.textBoxX.Location = new System.Drawing.Point(724, 207);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(75, 26);
+            this.textBoxX.TabIndex = 16;
+            this.textBoxX.Text = "1";
+            this.textBoxX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxX.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.BackColor = System.Drawing.Color.Snow;
+            this.textBoxY.Font = new System.Drawing.Font("Source Code Pro", 10.8F);
+            this.textBoxY.Location = new System.Drawing.Point(724, 273);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(75, 26);
+            this.textBoxY.TabIndex = 16;
+            this.textBoxY.Text = "1";
+            this.textBoxY.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxY.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(854, 631);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonEnd);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.buttonStart);
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MinimumSize = new System.Drawing.Size(870, 648);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonResult;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Example/Tromino谜题/vs-From/Tromino/Tromino/Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tromino
+{
+    public partial class Form1 : Form
+    {
+        private bool caFlag = false;//bool类型判断颜色是否已经计算
+        private int i = 1;//为棋盘上色时记数
+        private int size = 4;//默认棋盘长度为4
+        private int ptx = 1;//默认缺块的x坐标为1
+        private int pty = 1;//默认缺块的y坐标为1
+        private float sides = 100;//每个小格子的边长
+        private int Lc= 0;
+        private Graphics graphics;//图像类
+        private PointF[] Board;//从Board[1]开始，每三个为一组，表示L型骨牌,[1][2][3]为同一个L型骨牌的坐标
+        //Brushes笔刷，设置为系统定义颜色的Brush 对象，存放L型骨牌颜色
+        private Brush[] brushes ={Brushes.AliceBlue,Brushes.Aquamarine,Brushes.Aqua,Brushes.Azure,
+                                  Brushes.BlanchedAlmond, Brushes.Blue,Brushes.BurlyWood,Brushes.Beige,Brushes.BlueViolet,
+                                   Brushes.Chartreuse,Brushes.Crimson,Brushes.Cornsilk,Brushes.Coral,Brushes.CadetBlue,
+                                   Brushes.DarkKhaki,Brushes.DeepSkyBlue, Brushes.DarkOrchid,Brushes.DimGray, Brushes.DarkGoldenrod,                           
+                                   Brushes.ForestGreen,Brushes.Goldenrod, Brushes.Gainsboro,Brushes.GhostWhite,
+                                   Brushes.Honeydew, Brushes.IndianRed, Brushes.Indigo,Brushes.Ivory,Brushes.Khaki,
+                                   Brushes.LightGray,Brushes.LightCoral,Brushes.Linen, Brushes.Lavender,
+                                   Brushes.MediumPurple,Brushes.OliveDrab,Brushes.Orchid,Brushes.OrangeRed,
+                                   Brushes.PaleGoldenrod,Brushes.Purple,Brushes.PaleTurquoise,       
+          };
+        private const float CHESSBOARD_SIZE = 512;//棋盘尺寸
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = this.CreateGraphics(); //用于绘图的图像类
+            domainUpDown1.Tag = true;
+            textBoxX.Tag = true;
+            textBoxY.Tag = true;
+            Board = new PointF[size * size];
+            Board[0] = new PointF(1, 1);//先初始化第0个，后续从1开始计数，每三个数算一个L型骨牌
+            timer1.Enabled = false;
+            this.buttonPrev.Enabled = false;
+            this.buttonNext.Enabled = false;
+        }
+
+        //初始化form视图
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            drawFrame();
+        }
+
+        /**覆盖棋盘
+         * 通过缩小棋盘的size来改变棋盘大小，并用坐标定位的方式判断在几象限
+         * 用缺块的坐标与象限坐标范围比较，判断缺块的位置，优先填充不含缺块的位置，使之构成L型
+         * pointF,从Board[1]开始，每三个为一组，表示L型骨牌,[1][2][3]为同一个L型骨牌的坐标
+         * sides = 100;//每个小格子的边长
+         * 棋盘左下角的坐标(tx,ty)，缺块的坐标（dx,dy)
+         * Lc代表使用L型骨牌个数，减去缺块，为（4~k-1）/3
+         */
+        private void fillChessboard(int tx, int ty, int dx, int dy, int size)
+        {
+            if (size == 1)//当且仅当size=1时，只有一个方块，是缺块
+                return;
+            ++Lc;
+            int code = Lc;//骨牌号
+            int i = 0;
+            size = size / 2;
+            //缺块在此象限,覆盖第二象限的子棋盘
+            if (dx < tx + size && dy < ty + size)
+            {
+                //递归处理子棋盘直到左上角的子棋盘只剩一个时停止划分
+                fillChessboard(tx, ty, dx, dy, size);
+            }
+            else
+            {
+                //右下角方块坐标放入棋盘表中，将子棋盘中相应位置设置为骨牌号，将无特殊方块的子棋盘转换为含有缺块的，此时无特殊方块的子棋盘各自的缺块组成一个新的L型骨牌
+                Board[(code - 1) * 3 + (++i)] = new PointF(11 + (tx + size - 2) * sides, 11 + (ty + size - 2) * sides);
+                //递归覆盖其余方格
+                fillChessboard(tx, ty, tx + size - 1, ty + size - 1, size);
+            }
+
+            //缺块在此象限,覆盖第一象限的子棋盘
+            if (dx >= tx + size && dy < ty + size)
+            {
+                fillChessboard(tx + size, ty, dx, dy, size);
+            }
+            else
+            {
+                Board[(code - 1) * 3 + (++i)] = new PointF(11 + (tx + size - 1) * sides, 11 + (ty + size - 2) * sides);
+                fillChessboard(tx + size, ty, tx + size, ty + size - 1, size);
+            }
+
+            //缺块在此象限,覆盖第三象限的子棋盘
+            if (dx < tx + size && dy >= ty + size)
+            {
+                fillChessboard(tx, ty + size, dx, dy, size);
+            }
+            else
+            {
+                Board[(code - 1) * 3 + (++i)] = new PointF(11 + (tx + size - 2) * sides, 11 + (ty + size - 1) * sides);
+                fillChessboard(tx, ty + size, tx + size - 1, ty + size, size);
+            }
+
+            //缺块在此象限,覆盖第四象限的子棋盘
+            if (dx >= tx + size && dy >= ty + size)
+            {
+                fillChessboard(tx + size, ty + size, dx, dy, size);
+            }
+            else
+            {
+                Board[(code - 1) * 3 + (++i)] = new PointF(11 + (tx + size - 1) * sides, 11 + (ty + size - 1) * sides);
+                //Console.WriteLine("i = " + i.ToString());
+                fillChessboard(tx + size, ty + size, tx + size, ty + size, size);
+            }
+
+        }
+
+        //绘画
+        private void drawFrame()
+        {
+            size = Convert.ToInt32(this.domainUpDown1.Text);
+            sides = CHESSBOARD_SIZE / size;
+            graphics.FillRectangle(Brushes.Gray, new RectangleF(10, 10, CHESSBOARD_SIZE, CHESSBOARD_SIZE));
+            Pen pen = new Pen(Color.Black, 1);
+            for (int i = 0; i <= size; ++i)
+            {
+                graphics.DrawLine(pen, 10 + i * sides, 10, 10 + i * sides, CHESSBOARD_SIZE + 10);
+                graphics.DrawLine(pen, 10, 10 + i * sides, CHESSBOARD_SIZE + 10, 10 + i * sides);
+            }
+            graphics.FillRectangle(Brushes.Pink, new RectangleF(11 + (ptx - 1) * sides, 11 + (pty - 1) * sides, sides - 1, sides - 1));
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i < size * size)
+            {
+                graphics.FillRectangle(brushes[i % 5], Board[i].X, Board[i].Y, sides - 1f, sides - 1f);
+                graphics.FillRectangle(brushes[i % 5], Board[i + 1].X, Board[i + 1].Y, sides - 1f, sides - 1f);
+                graphics.FillRectangle(brushes[i % 5], Board[i + 2].X, Board[i + 2].Y, sides - 1f, sides - 1f);
+                i = i + 3;
+            }
+            else
+            {
+                i = 1;
+                this.buttonStart.Text = "start";
+                timer1.Enabled = false;
+                this.domainUpDown1.Enabled = true;
+                this.textBoxX.Enabled = true;
+                this.textBoxY.Enabled = true;
+            }
+
+        }
+
+  
+        //开始按钮事件
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (this.buttonStart.Text == "start")
+            {
+                drawFrame();
+                i = 1;
+                Lc = 0;
+                this.buttonStart.Enabled = false;
+                if (caFlag == false)
+                {
+                    Board = new PointF[size * size];
+                    fillChessboard(1, 1, ptx, pty, size);
+                    caFlag = true;
+                }
+                this.buttonStart.Text = "suspend";
+                this.buttonStart.Enabled = true;
+                timer1.Enabled = true;
+                this.domainUpDown1.Enabled = false;
+                this.textBoxX.Enabled = false;
+                this.textBoxY.Enabled = false;
+                this.buttonResult.Enabled = true;
+                this.buttonPrev.Enabled = true;
+                this.buttonNext.Enabled = true;
+            }
+            else if (this.buttonStart.Text == "suspend")
+            {
+                this.buttonStart.Text = "continue";
+                timer1.Enabled = false;
+
+            }
+            else if (this.buttonStart.Text == "continue")
+            {
+                this.buttonStart.Text = "suspend";
+                timer1.Enabled = true;
+            }
+        }
+
+        //上一步按钮事件
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (i >= 3)
+            {
+                i -= 3;
+                graphics.FillRectangle(Brushes.PowderBlue, Board[i].X, Board[i].Y, sides - 1, sides - 1);
+                graphics.FillRectangle(Brushes.PowderBlue, Board[i + 1].X, Board[i + 1].Y, sides - 1, sides - 1);
+                graphics.FillRectangle(Brushes.PowderBlue, Board[i + 2].X, Board[i + 2].Y, sides - 1, sides - 1);
+            }
+        }
+
+        //下一步按钮事件
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (i <= size * size - 3)
+            {
+                graphics.FillRectangle(brushes[i % 20], Board[i].X, Board[i].Y, sides - 1, sides - 1);
+                graphics.FillRectangle(brushes[i % 20], Board[i + 1].X, Board[i + 1].Y, sides - 1, sides - 1);
+                graphics.FillRectangle(brushes[i % 20], Board[i + 2].X, Board[i + 2].Y, sides - 1, sides - 1);
+                i += 3;
+            }
+        }
+
+        //重新开始按钮事件
+        private void buttonEnd_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            drawFrame();
+            this.buttonStart.Text = "start";
+            i = 1;
+            Lc = 0;
+            this.domainUpDown1.Enabled = true;
+            this.textBoxX.Enabled = true;
+            this.textBoxY.Enabled = true;
+        }
+
+        //结果按钮事件
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            i = 1;
+            Lc = 0;
+            if (caFlag == false)
+            {
+                Board = new PointF[size * size];
+                fillChessboard(1, 1, ptx, pty, size);
+                caFlag = true;
+            }
+            while (i <= size * size - 3)
+            {
+                graphics.FillRectangle(brushes[i % 20], Board[i].X, Board[i].Y, sides - 1, sides - 1);
+                graphics.FillRectangle(brushes[i % 20], Board[i + 1].X, Board[i + 1].Y, sides - 1, sides - 1);
+                graphics.FillRectangle(brushes[i % 20], Board[i + 2].X, Board[i + 2].Y, sides - 1, sides - 1);
+                i += 3;
+            }
+            this.buttonPrev.Enabled = true;
+            this.buttonNext.Enabled = true;
+            this.domainUpDown1.Enabled = true;
+            this.textBoxX.Enabled = true;
+            this.textBoxY.Enabled = true;
+            this.buttonStart.Text = "start";
+        }
+
+
+        private void resultEvent()
+        {
+            timer1.Enabled = false;
+            i = 1;
+            Lc = 0;
+            if (caFlag == false)
+            {
+                Board = new PointF[size * size];
+                fillChessboard(1, 1, ptx, pty, size);
+                caFlag = true;
+            }
+            while (i <= size * size - 3)
+            {
+                graphics.FillRectangle(brushes[i % 20], Board[i].X, Board[i].Y, sides - 1, sides - 1);
+                graphics.FillRectangle(brushes[i % 20], Board[i + 1].X, Board[i + 1].Y, sides - 1, sides - 1);
+                graphics.FillRectangle(brushes[i % 20], Board[i + 2].X, Board[i + 2].Y, sides - 1, sides - 1);
+                i += 3;
+            }
+            this.buttonPrev.Enabled = true;
+            this.buttonNext.Enabled = true;
+            this.domainUpDown1.Enabled = true;
+            this.textBoxX.Enabled = true;
+            this.textBoxY.Enabled = true;
+            this.buttonStart.Text = "start";
+        }
+     
+        //设置棋盘的长度
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+            this.buttonPrev.Enabled = false;
+            this.buttonNext.Enabled = false;
+            caFlag = false;
+            DomainUpDown tb = (DomainUpDown)sender;
+            //如果不是空
+            if (tb.Text != "")
+            {
+                int num = Convert.ToInt32(tb.Text);
+                //num必须是2的幂
+                if ((num != 0 && (num & (num - 1)) == 0)&& num >= Convert.ToInt16(this.textBoxX.Text) && num >= Convert.ToInt16(this.textBoxY.Text))  
+                {
+                    //如果size，x，y 都有效 ，tag 为 true ，开始绘图
+                    tb.Tag = true;
+                    this.textBoxX.Tag = true;
+                    this.textBoxY.Tag = true;
+                    tb.BackColor = System.Drawing.SystemColors.Window;
+                    vStart();
+                    return;
+                }
+            }
+            tb.Tag = false;
+            vStart();
+        }
+
+        //缺块的X,Y处理
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            this.buttonPrev.Enabled = false;
+            this.buttonNext.Enabled = false;
+            caFlag = false;
+            TextBox tb = (TextBox)sender;
+            if (tb.Text == "")
+            {
+                tb.Tag = false;
+            }
+            else if (Convert.ToInt32(tb.Text) > Convert.ToInt32(this.domainUpDown1.Text)||Convert.ToInt32(tb.Text)<=0)
+            {
+                tb.Tag = false;
+                MessageBox.Show("错误！请修改初始块的坐标！");
+                this.buttonStart.Enabled = false;
+            }
+            else
+            {
+                if (tb == this.textBoxX)
+                {
+                    ptx = Convert.ToInt32(tb.Text);
+                }
+                if (tb == this.textBoxY)
+                {
+                    pty = Convert.ToInt32(tb.Text);
+                }
+                tb.Tag = true;
+            }
+            vStart();
+        }
+
+
+        private void vStart()
+        {
+            this.buttonEnd.Enabled = this.buttonResult.Enabled
+                = this.buttonStart.Enabled
+                = (bool)domainUpDown1.Tag && (bool)textBoxX.Tag && (bool)textBoxY.Tag;
+            if (this.buttonStart.Enabled == true)
+                drawFrame();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.domainUpDown1.SelectedItem = this.domainUpDown1.Items[5];
+            vStart();
+            this.domainUpDown1.SelectedItem = this.domainUpDown1.Items[5];
+        }
+    }
+}
